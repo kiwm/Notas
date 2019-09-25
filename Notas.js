04124d3d51e21a4d44a909ce146913ba5542bbab
kiwm <pedromaltapedrozo@gmail.com>
2019-09-25 18:03:31 -0300

    funcao de deletar nota adicionada

--- Notas.js
+++ Notas.js
@@ -1,7 +1,7 @@
  var list = document.querySelector('ul#list')
  var txt = document.querySelector('input#txt')
  var button = document.querySelector('input#btn')
-
+ button.onclick = addTodo;
  var todos = []
 
  function renderTodos() {
@@ -9,12 +9,25 @@
      for(todo of todos) {
          var todoElement = document.createElement('li')
          var text = document.createTextNode(todo)
+         var link = document.createElement('a')
+
+        link.setAttribute('href', '#')
+        var pos = todos.indexOf(todo)
+        link.setAttribute('onclick', 'deleteTodo('+ pos +')')
+         var linkText = document.createTextNode('Excluir')
+         link.appendChild(linkText)
+
 
          todoElement.appendChild(text)
+         todoElement.appendChild(link)
          list.appendChild(todoElement)
      }
  }
 
+ function deleteTodo(pos) {
+    todos.splice(pos, 1)
+    renderTodos()
+ }
 
  function addTodo() {
        var text = txt.value
@@ -22,7 +35,7 @@
        txt.value=''
     renderTodos()
  }
- button.onclick = addTodo;
+
 
 
  
